39b22e5fbe07165639ff9f2d2dff81c3b5a41b09
naz <nazkrdmr00@gmail.com>
2024-12-11 12:01:57 +0300

    a
    a

--- project/static/script.js
+++ project/static/script.js
@@ -1,16 +1,14 @@
 // Form doğrulama işlevi
 function validateForm(form) {
-    let username = form.username.value.trim();
+    let username = form.username?.value.trim();
     let password = form.password.value.trim();
 
-    // Giriş Formu Doğrulaması
-    if (username === "" || password === "") {
+    if (!username && !form.confirm_password) {
         alert("Lütfen kullanıcı adı ve şifreyi girin.");
         return false;
     }
 
-    // Kayıt Formu Doğrulaması
-    if (form.confirm_password) { // Kayıt formunda var mı kontrol et
+    if (form.confirm_password) {
         let confirmPassword = form.confirm_password.value.trim();
         if (password !== confirmPassword) {
             alert("Şifreler uyuşmuyor.");
@@ -21,27 +19,28 @@ function validateForm(form) {
     return true;
 }
 
-// Şifre doğrulama
-const form = document.querySelector('form');
-const password = document.getElementById('password');
-const confirmPassword = document.getElementById('confirm_password');
+// Login formu
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    loginForm.addEventListener('submit', function(event) {
+        if (!validateForm(this)) {
+            event.preventDefault();
+        }
+    });
+}
 
-form.addEventListener('submit', function (e) {
-    if (password.value !== confirmPassword.value) {
-        e.preventDefault();
-        alert('Şifreler eşleşmiyor. Lütfen tekrar kontrol edin.');
-    }
-});
+// Register formu
+const registerForm = document.getElementById('registerForm');
+if (registerForm) {
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
 
-// Form submit olayını dinler ve doğrulamayı gerçekleştirir
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    if (!validateForm(this)) {
-        event.preventDefault(); // Submit iptal edilir
-    }
-});
-
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    if (!validateForm(this)) {
-        event.preventDefault(); // Submit iptal edilir
-    }
-});
+    registerForm.addEventListener('submit', function(event) {
+        if (password.value !== confirmPassword.value) {
+            event.preventDefault();
+            alert('Şifreler eşleşmiyor. Lütfen tekrar kontrol edin.');
+        } else if (!validateForm(this)) {
+            event.preventDefault();
+        }
+    });
+}
